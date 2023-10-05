70bf2e8d756409d21dd8d5f541a34e3e2164d3af
Ihar Androsovich <igor.dekster@mail.ru>
2023-10-06 00:01:42 +0300

    added in module drivers  GrpcService

--- services/drivers/src/main/java/org/androsovich/drivers/services/grpc/DriverCarServerService.java
+++ services/drivers/src/main/java/org/androsovich/drivers/services/grpc/DriverCarServerService.java
@@ -1,15 +1,19 @@
 package org.androsovich.drivers.services.grpc;
 
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.androsovich.Car;
 import org.androsovich.Driver;
 import org.androsovich.DriverCarServiceGrpc;
 
 @GrpcService
+@Slf4j
 public class DriverCarServerService extends DriverCarServiceGrpc.DriverCarServiceImplBase {
     @Override
     public void getCarByDriver(Driver request, StreamObserver<Car> responseObserver) {
+        log.info("inside method getCarByDriver :: DriverCarServerService");
         super.getCarByDriver(request, responseObserver);
+        responseObserver.onCompleted();
     }
 }
