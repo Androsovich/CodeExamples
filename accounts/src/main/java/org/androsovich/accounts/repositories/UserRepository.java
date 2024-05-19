494fc8a57eb7990dcb80ac0c2e55e53e058b707a
Ihar Androsovich <igor.dekster@mail.ru>
2024-05-19 23:12:39 +0300

    added new methods to UserRepository

--- accounts/src/main/java/org/androsovich/accounts/repositories/UserRepository.java
+++ accounts/src/main/java/org/androsovich/accounts/repositories/UserRepository.java
@@ -10,4 +10,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserName(String userName);
+
+    User findByEmail(String email);
+
+    User findByPhone(String phone);
 }
