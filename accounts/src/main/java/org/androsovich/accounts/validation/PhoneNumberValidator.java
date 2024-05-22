502f91797f6819dc040437aa49b6effd4e21a9c0
Ihar Androsovich <igor.dekster@mail.ru>
2024-05-22 20:12:47 +0300

    added annotation to validate phone

---
+++ accounts/src/main/java/org/androsovich/accounts/validation/PhoneNumberValidator.java
@@ -0,0 +1,19 @@
+package org.androsovich.accounts.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PhoneNumberValidator implements ConstraintValidator<Phone, String> {
+    @Override
+    public boolean isValid(String phone, ConstraintValidatorContext constraintValidatorContext) {
+        String regexPhones = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
+                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
+        return phone != null && phone.matches(regexPhones);
+    }
+
+    @Override
+    public void initialize(Phone constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+}
