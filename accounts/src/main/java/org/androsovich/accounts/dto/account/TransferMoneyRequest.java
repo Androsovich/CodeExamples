4bd3b221e2f58b99cbbfd79b0425ad6ac09e6950
Ihar Androsovich <igor.dekster@mail.ru>
2024-05-23 21:53:37 +0300

    added transfer DTO

---
+++ accounts/src/main/java/org/androsovich/accounts/dto/account/TransferMoneyRequest.java
@@ -0,0 +1,25 @@
+package org.androsovich.accounts.dto.account;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferMoneyRequest {
+    @NotNull
+    long fromAccountId;
+
+    @NotNull
+    long toAccountId;
+
+    @NotNull
+    @Min(0)
+    BigDecimal balance;
+}
